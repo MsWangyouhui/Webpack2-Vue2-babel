cab51e08b8900abbee3098b7638154ca3a045ecb
MsWangyouhui <ponianrsheng@foxmail.com>
2017-08-18 19:01:55 +0800

    first commit

---
+++ public/index.js
@@ -0,0 +1,14 @@
+//import "../assecc/css/style.css";
+// import  '../assecc/css/style.css';
+import Vue from 'vue';
+/* ( there may be other imports here ) */
+// import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+// import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+/* ( there may be other imports here ) */
+
+// Vue.use(BootstrapVue);
+
+import bulid from "../bulid/bulid";
+
+new Vue(bulid);
---
+++ webpack.unless.js
@@ -0,0 +1,57 @@
+'use strict'
+
+
+const HtmlWebpackPlugin=require("html-webpack-plugin");
+const webpack = require('webpack');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path');
+const config = {
+  entry: ['../public/index.js'],
+  output: {
+    path: path.resolve(__dirname, '../index/'),
+    filename: '[name].[hash].js',
+  },
+  module: {
+    rules:[
+      {
+        test: /\.js$/,
+        include:[path.resolve(__dirname,'/')],
+        loader: 'babel-loader',
+        exclude: [new RegExp(`node_modules\\${path.sep}(?!vue-bulma-.*)`)]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+    
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          name:  path.posix.join('static', 'img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: path.posix.join('static', 'media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: path.posix.join('static', 'fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.vue','.js', 'json']
+  },
+};
+
+module.exports = config;
